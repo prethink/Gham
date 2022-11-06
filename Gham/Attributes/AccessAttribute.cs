c246db4612fe29c07321c3d0f70d927b4607b8de
Ilya Samarin <ilia-samarin@bk.ru>
2022-11-06 10:29:39 +0700

    Добавлена возможность проверки методов с помощью атрибута Access

---
+++ Gham/Attributes/AccessAttribute.cs
@@ -0,0 +1,22 @@
+﻿using Gham.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gham.Attributes
+{
+    internal class AccessAttribute : Attribute
+    {
+        public UserPrivilege? RequiredPrivilege { get; set; }
+        public AccessAttribute()
+        {
+        }
+
+        public AccessAttribute(UserPrivilege privilages)
+        {
+            RequiredPrivilege = privilages;
+        }
+    }
+}
